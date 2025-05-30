1d94450358255c9cb88bc16a31d010c5916a87f5
Jacobo Hernandez Varela <48144152+JacobHdez@users.noreply.github.com>
2025-05-29 23:04:37 -0600

    feat: show drivers predictions

--- api/historical/routes.py
+++ api/historical/routes.py
@@ -1,8 +1,15 @@
 from flask import render_template
+import pandas as pd
 
 from api.historical import historical_bp
 
 
 @historical_bp.route('/', methods=('GET',))
 def index():
-    return render_template('historical.html')
+    df = pd.read_csv('api/static/f1_churn_predict_final.csv', parse_dates=['dtRef'])
+    results = {
+        date: group[['idDriver', 'f1Churn']].to_dict('records')
+        for date, group in df.groupby('dtRef')
+    }
+
+    return render_template('historical.html', results=results)
