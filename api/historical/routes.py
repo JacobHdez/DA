1b501eea757e2eabd2037a042dc66feb02b17623
Jacobo Hernandez Varela <48144152+JacobHdez@users.noreply.github.com>
2025-05-29 21:16:54 -0600

    feat: add historical html

--- api/historical/routes.py
+++ api/historical/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 from api.historical import historical_bp
 
 
 @historical_bp.route('/', methods=('GET',))
 def index():
-    return 'Hello, World!!! from Historical'
+    return render_template('historical.html')
